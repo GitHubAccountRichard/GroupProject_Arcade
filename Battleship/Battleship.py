ec98b1824184bb4d19a3337f26a3e6eaf43da238
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-02-20 12:11:34 +0100

    Added function which creates 10 rows as well as code to define those rows as a board, made them lists because they are mutable and idk if we can use matrix

--- Battleship/Battleship.py
+++ Battleship/Battleship.py
@@ -1,1 +1,24 @@
-#This
+#First need to define a function which gives us 10 lists with 10 spaces (10x10) matrix
+
+def battleship_board():
+    Row_1 = ["[ ]"] * 10
+    Row_2 = ["[ ]"] * 10
+    Row_3 = ["[ ]"] * 10
+    Row_4 = ["[ ]"] * 10
+    Row_5 = ["[ ]"] * 10
+    Row_6 = ["[ ]"] * 10
+    Row_7 = ["[ ]"] * 10
+    Row_8 = ["[ ]"] * 10
+    Row_9 = ["[ ]"] * 10
+    Row_10 = ["[ ]"] * 10
+    return [Row_1, Row_2, Row_3, Row_4, Row_5, Row_6, Row_7, Row_8, Row_9, Row_10]
+
+
+
+
+board = battleship_board()
+for row in board:
+    print(" ".join(row))
+
+
+
