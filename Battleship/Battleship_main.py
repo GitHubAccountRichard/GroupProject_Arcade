936371ad9a51906ec0a9a8c35a5a09d6aff01263
GitHubAccountRichard <rglatter.ieu2023@student.ie.edu>
2025-02-21 22:11:24 +0100

    Actually importing the correct file now and not just the module

--- Battleship/Battleship_main.py
+++ Battleship/Battleship_main.py
@@ -1,4 +1,4 @@
-import Battleship
+from Battleship.Battleship import makeBoard, putShip, print_board, user_input, check_hit
 
 
 def game():
@@ -6,10 +6,10 @@ def game():
     print("Welcome to Battleship!")
 
     # Create board
-    board = Battleship.makeBoard(size=7)
+    board = makeBoard(size=7)
 
     # Place the ship
-    Battleship.putShip(board, size=4)
+    putShip(board, size=4)
 
     # how many parts are left to hit
     ship_left = 4
@@ -17,13 +17,13 @@ def game():
     # loopiung the game
     while ship_left > 0:
         # Display the current board, hide true because we dont want to sere the board
-        Battleship.print_board(board, hide=True)
+        print_board(board, hide=True)
 
         # player's guess
-        guess = Battleship.user_input()
+        guess = user_input()
 
         # Check the guess
-        result = Battleship.check_hit(board, guess[0], guess[1])
+        result = check_hit(board, guess[0], guess[1])
         print(result["message"])
 
         if result["hit_state"] is True:  # Hit
@@ -32,7 +32,7 @@ def game():
     # Player has sunk the ship
     print("\nYou sank the ship!")
     # Reveal board with ship positions
-    Battleship.print_board(board, hide=False)
+    print_board(board, hide=False)
 
 
 #Still dont understand this but copy and past from his slides and seems to work ¯\_(ツ)_/¯
