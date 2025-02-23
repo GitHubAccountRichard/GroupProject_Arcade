271431f686c69cbcd2a1d4d4fa577a3079f04b7d
GitHubAccountRichard <rglatter.ieu2023@student.ie.edu>
2025-02-23 19:27:00 +0100

    Wordle.py works now and account for double letters in a word, also added wordle game to main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 from Battleship import Battleship_main
 from Battleship import Battleship_functions
 from Hangman.hangaman_alt import hangman_game
+from Wordle.Wordle import wordle_game
 
 
 
 
-
-Games = ["1. Battleship", "2. Hangman", "3. Roulette", "4. TicTacToe", "5. Quit"]
+Games = ["1. Battleship", "2. Hangman", "3. Roulette", "4. TicTacToe", "5. Wordle", "6. Quit"]
 print("Welcome to the Arcade!")
-print("We have a couple of game")
 for i in Games:
     print(i)
 
 while True:
     try:
-        GameChoice = int(input("What game do you want to play? (1-5)"))
-        if 1 <= GameChoice <= 5:
+        GameChoice = int(input("What game do you want to play? (1-6)"))
+        if 1 <= GameChoice <= 6:
             break
         else:
-            print("Stop messing around - please enter a number between 1 and 5")
+            print("Stop messing around - please enter a number between 1 and 6")
     except ValueError:
-        print("Stop messing around - please enter a valid number between 1 and 5")
+        print("Stop messing around - please enter a valid number between 1 and 6")
     
     
 if GameChoice == 1:
@@ -31,4 +30,7 @@ elif GameChoice == 2:
     if __name__ == "__main__":
         hangman_game()
 elif GameChoice == 5:
+    if __name__ == "__main__":
+        wordle_game()
+else:
     quit()
