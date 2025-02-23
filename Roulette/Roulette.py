c9cb8d503cb16b07b8a1bb8c95ba1d0c57b4fff3
teosierrar <teosierram@gmail.com>
2025-02-23 12:41:16 +0100

    Roulette progress

--- Roulette/Roulette.py
+++ Roulette/Roulette.py
@@ -21,9 +21,17 @@ while True:
         break
     else:
         print(f"Invalid bet! Your bet cannot exceed your credit (${credit}). Please try again.")
-
+first12=list(range(1, 13))
+second12=list(range(13, 25))
+third12=list(range(25, 37))
+row1=[i for i in range(1, 37) if (i - 1) % 3 == 0]
+row2=[i for i in range(1,37) if (i-2)%3==0]
+row3=[i for i in range (1,37) if (i-3)%3==0]
+print(f"First 12 is {first12}, Second 12 is {second12}, Third 12 is {third12}.")
+print(f"Row 1 is {row1}, Row 2 is {row2}, Row 3 is {row3}.")
+print(f"Black is even, red is odd.")
 print(f"We are not responsible for any losses due to lack of following the rules.")
-numbet=(input("Enter where you want to bet (options are 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, red, black, green): "))
+numbet=(input("Enter where you want to bet (options are 0 to 36, red, black, first12, second12, third12, row1, row2, row3): "))
 if numbet.isdigit():
     numbet=int(numbet)
 else:
@@ -40,6 +48,26 @@ elif numbet==roulette_choice:
     print("You won!")
     credit += bet*36
     print(f"You won ${bet*36}!")
+elif numbet is first12:
+    if roulette_choice in first12:
+        print("You won!")
+        credit += bet*3
+        print(f"You won ${bet*3}!")
+elif numbet is second12:
+    if roulette_choice in second12:
+        print("You won!")
+        credit += bet*3
+        print(f"You won ${bet*3}!")
+elif numbet is third12:
+    if roulette_choice in third12:
+        print("You won!")
+        credit += bet*3
+        print(f"You won ${bet*3}!")
+elif numbet is row1:
+    if roulette_choice in row1:
+        print("You won!")
+        credit += bet*3
+        print(f"You won ${bet*3}!")
 else:
     print("You lost!")
     credit -= bet
