14e42c6f5604e546827f3345e8ef65395e719094
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-02-23 10:40:53 +0100

    Added some more functionality to the Wordle Game

--- Wordle/Wordle.py
+++ Wordle/Wordle.py
@@ -1,22 +1,27 @@
-#File to add a Wordle game
-
-
-#First need to import an online dictionary
-import requests
+# First need to import an online dictionary
 import random
+
+import requests
 
 book = requests.get("https://www.mit.edu/~ecprice/wordlist.10000")
 lines = book.text.split("\n")
-number_of_words = len(lines)
-print(number_of_words)
 
 #Second get one random word from the dictionary
-random_word = lines[random.randint(0, number_of_words-1)]
-print(random_word)
+five_letter_words = []
+for word in lines:
+    if len(word) == 5:
+        five_letter_words.append(word)
 
-word_guess_list = ["_"] * len(random_word)
+five_letter_words = five_letter_words[random.randint(0, len(five_letter_words))]
+
+word_guess_list = ["_"] * len(five_letter_words)
 print(word_guess_list)
 
 #Write a function that takes the input and checks against the user input
+#Get user input
+guess = input("Enter a five letter word: ")
 
-
+for i in range(len(five_letter_words)):
+    if guess[i] in five_letter_words:
+        word_guess_list[i] = guess[i]
+        print(word_guess_list)
