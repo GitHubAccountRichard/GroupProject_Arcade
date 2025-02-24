2bf8afe9f77df0cfb62072c316e056a9cdca9193
Richard Glatter <rglatter.ieu2023@student.ie.edu>
2025-02-24 13:20:54 +0100

    Looping the Games so you can keep playing

--- main.py
+++ main.py
@@ -1,47 +1,43 @@
-from Battleship import Battleship_main
-from Battleship import Battleship_functions
-from Hangman.hangaman_alt import hangman_game
-from Wordle.Wordle import wordle_game
-from Roulette.Roulette import Roulette_game
-from time import *
 import Blackjack.Blackjack_game as Blackjack_game_funciton
+from Battleship import Battleship_main
+from Hangman.hangaman_alt import hangman_game
+from Roulette.Roulette import Roulette_game
 from TicTacToe.TicTacToe import play_game
+from Wordle.Wordle import wordle_game
 
+Games = ["1. Battleship", "2. Hangman", "3. Roulette", "4. Wordle", "5. Blackjack", "6. Tic-Tac-Toe", "7. Memory Game",
+         "8. Quit"]
 
-
-Games = ["1. Battleship", "2. Hangman", "3. Roulette", "4. Wordle", "5. Blackjack","6. Tic-Tac-Toe", "7. Memory Game", "8. Quit"]
-print("Welcome to the Arcade!")
-for i in Games:
-    print(i)
 
 while True:
+    for i in Games:
+        print(i)
+
     try:
         GameChoice = int(input("What game do you want to play? (1-8)"))
-        if 1 <= GameChoice <= 6:
-            break
+        if 1 <= GameChoice <= 8:
+            if GameChoice == 8:
+                print("Thank you for playing! Goodbye!")
+                break
+            elif GameChoice == 1:
+                if __name__ == "__main__":
+                    Battleship_main.game()
+            elif GameChoice == 2:
+                if __name__ == "__main__":
+                    hangman_game()
+            elif GameChoice == 3:
+                if __name__ == "__main__":
+                    Roulette_game()
+            elif GameChoice == 4:
+                if __name__ == "__main__":
+                    wordle_game()
+            elif GameChoice == 5:
+                if __name__ == "__main__":
+                    Blackjack_game_funciton()
+            elif GameChoice == 6:
+                if __name__ == "__main__":
+                    play_game()
         else:
             print("Stop messing around - please enter a number between 1 and 8")
     except ValueError:
         print("Stop messing around - please enter a valid number between 1 and 8")
-    
-    
-if GameChoice == 1:
-    if __name__ == "__main__": #I forgot what this does but he mentioned that this is good so use it for your games too I guess ¯\_(ツ)_/¯
-        Battleship_main.game()
-elif GameChoice == 2:
-    if __name__ == "__main__":
-        hangman_game()
-elif GameChoice == 3:
-    if __name__ == "__main__":
-        Roulette_game()
-elif GameChoice == 4:
-    if __name__ == "__main__":
-        wordle_game()
-elif GameChoice == 5:
-    if __name__ == "__main__":
-        Blackjack_game_funciton()
-elif GameChoice == 6:
-    if __name__ == "__main__":
-        play_game()
-else:
-    quit()
